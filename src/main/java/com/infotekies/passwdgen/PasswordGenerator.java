3793c7124d2d3a78827b2ec2830f97a15e7b8ba3
sneethiraj <sneethir@apache.org>
2016-02-29 01:08:26 -0500

    modified code to parameters in prop file

--- src/main/java/com/infotekies/passwdgen/PasswordGenerator.java
+++ src/main/java/com/infotekies/passwdgen/PasswordGenerator.java
@@ -4,36 +4,48 @@ package com.infotekies.passwdgen;
  * (c) 2011, InfoTekies Corporation
  */
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Properties;
 import java.util.Random;
 
 public class PasswordGenerator {
-
-	/**
-	 * @param args
-	 */
 	
-	private static final int DEFAULT_NO_PASSWORD_TO_GENRATE = 100 ;
+	public static final String CONFIG_FILENAME = "/passwordgen.properties" ;
 	
-	private int minimumPasswordLength = 8 ;
+	public static final String CONFIG_PREFIX = "generate.password." ;
 	
-	private int maximumPasswordLength = 12 ;
+	public static final String GEN_PASSWORD_COUNT_CONFIG_NAME = "count" ;
+	public static final String MIN_LEN_CONFIG_NAME = "minLen" ;
+	public static final String MAX_LEN_CONFIG_NAME = "maxLen" ;
+	public static final String NUMERIC_NEEDED_CONFIG_NAME = "numericNeeded" ;
+	public static final String BOTH_CASE_NEEDED_CONFIG_NAME = "bothCaseNeeded" ;
 	
-	private boolean isExpectedNumberic = true ;
 	
-	private boolean isExpectedBothCase = true ;
+	public static final int DEFAULT_GEN_PASSWORD_COUNT = 10 ;
+	
+	public static final int DEFAULT_GEN_PASSWORD_MIN_LEN =   8 ;
+	public static final int DEFAULT_GEN_PASSWORD_MAX_LEN = 12 ;
+	public static final boolean DEFAULT_GEN_PASSWORD_NEED_NUMERIC = true ;
+	public static final boolean  DEFAULT_GEN_PASSWORD_NEED_BOTH_CASE = true ;
+	
 	
 	private static final ArrayList<Character> alphaLetters = new ArrayList<Character>() ;
-
 	private static final ArrayList<Character> alphaUpperLetters = new ArrayList<Character>() ;
-
 	private static final ArrayList<Character> numericLetters = new ArrayList<Character>() ;
+
+	private Properties prop = new Properties() ;
 	
-	
+	private  int noOfPasswordToGenerate =  DEFAULT_GEN_PASSWORD_COUNT ;
+	private int minimumPasswordLength = DEFAULT_GEN_PASSWORD_MIN_LEN ;
+	private int maximumPasswordLength = DEFAULT_GEN_PASSWORD_MAX_LEN ;
+	private boolean isExpectedNumberic = DEFAULT_GEN_PASSWORD_NEED_NUMERIC ;
+	private boolean isExpectedBothCase = DEFAULT_GEN_PASSWORD_NEED_BOTH_CASE ;
+
 	static {
 		for(int x = 'a' ; x <= 'z' ; x++) {
 			char v = (char)x ;
-			// System.err.println(v) ;
 			alphaLetters.add(Character.toLowerCase(v)) ;
 			alphaUpperLetters.add(Character.toUpperCase(v)) ;
 		}
@@ -42,23 +54,70 @@ public class PasswordGenerator {
  		}
 	}
 	
+	public void init() {
+		InputStream in = getClass().getResourceAsStream(CONFIG_FILENAME) ;
+		if (in != null) {
+			try {
+				prop.load(in);
+				
+				noOfPasswordToGenerate = getIntConfigValue(GEN_PASSWORD_COUNT_CONFIG_NAME, DEFAULT_GEN_PASSWORD_COUNT) ;
+				minimumPasswordLength = getIntConfigValue(MIN_LEN_CONFIG_NAME, DEFAULT_GEN_PASSWORD_MIN_LEN) ;
+				maximumPasswordLength = getIntConfigValue(MAX_LEN_CONFIG_NAME, DEFAULT_GEN_PASSWORD_MAX_LEN) ;
+				isExpectedNumberic =  "TRUE".equalsIgnoreCase(getConfigValue(NUMERIC_NEEDED_CONFIG_NAME,  Boolean.toString(DEFAULT_GEN_PASSWORD_NEED_NUMERIC))) ;
+				isExpectedBothCase  =  "TRUE".equalsIgnoreCase(getConfigValue(BOTH_CASE_NEEDED_CONFIG_NAME, Boolean.toString(DEFAULT_GEN_PASSWORD_NEED_BOTH_CASE))) ;
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
+	}
+	
+	public PasswordGenerator() {
+		init() ;
+	}
 
 	
-	public static void main(String[] args) {
-		
-		int noOfPasswordToGenerate = DEFAULT_NO_PASSWORD_TO_GENRATE ;
-		
-		if (args.length > 0) {
-			noOfPasswordToGenerate = Integer.parseInt(args[0]) ;
-		}
-		
-		PasswordGenerator pg = new PasswordGenerator() ;
-		
-		for(int i = 0 ; i < noOfPasswordToGenerate; i++) {
-			System.out.println(pg.generatorPassword()) ;
-		}
-		
+	public int getNoOfPasswordToGenerate() {
+		return noOfPasswordToGenerate;
 	}
+
+	public void setNoOfPasswordToGenerate(int noOfPasswordToGenerate) {
+		this.noOfPasswordToGenerate = noOfPasswordToGenerate;
+	}
+
+	public int getMinimumPasswordLength() {
+		return minimumPasswordLength;
+	}
+
+	public void setMinimumPasswordLength(int minimumPasswordLength) {
+		this.minimumPasswordLength = minimumPasswordLength;
+	}
+
+	public int getMaximumPasswordLength() {
+		return maximumPasswordLength;
+	}
+
+	public void setMaximumPasswordLength(int maximumPasswordLength) {
+		this.maximumPasswordLength = maximumPasswordLength;
+	}
+
+	public boolean isExpectedNumberic() {
+		return isExpectedNumberic;
+	}
+
+	public void setExpectedNumberic(boolean isExpectedNumberic) {
+		this.isExpectedNumberic = isExpectedNumberic;
+	}
+
+	public boolean isExpectedBothCase() {
+		return isExpectedBothCase;
+	}
+
+	public void setExpectedBothCase(boolean isExpectedBothCase) {
+		this.isExpectedBothCase = isExpectedBothCase;
+	}
+
 	
 	
 	private int getPasswordLength() {
@@ -76,7 +135,7 @@ public class PasswordGenerator {
 	}
 	
 	
-	public String generatorPassword() {
+	private String generatorPassword() {
 	
 		String password = null ;
 		
@@ -113,6 +172,13 @@ public class PasswordGenerator {
 		
 	}
 	
+	public void printGeneratedPasswords() {
+		for(int i = 0 ; i < noOfPasswordToGenerate; i++) {
+			System.out.println(generatorPassword()) ;
+		}
+	}
+
+	
 	private boolean isValidPassword(String pass) {
 		boolean ret = true ;
 		
@@ -145,5 +211,64 @@ public class PasswordGenerator {
 		
 		return ret ;
 	}
+	
+	public String getConfigValue(String key, String defaultValue) {
+		String ret = null ;
+		
+		ret = System.getProperty(key) ;
+		
+		if (ret == null) {
+			ret = prop.getProperty(key) ;
+		}
+		
+		if (! key.startsWith(CONFIG_PREFIX)) {
+			
+			String kn =  CONFIG_PREFIX + key ;
+			
+			ret = System.getProperty(kn) ;
+			
+			if (ret == null) {
+				ret = prop.getProperty(kn) ;
+			}	
+		}
+		
+		if (ret == null) {
+			ret = defaultValue ;
+		}
+		
+		return ret ;
+	}
+
+	public String getConfigValue(String key) {
+		return getConfigValue(key,null) ;
+	}
+
+	
+	public int getIntConfigValue(String key) {
+		return getIntConfigValue(key,0) ;
+	}
+	
+	public int getIntConfigValue(String key, int defaultValue) {
+		int retVal = defaultValue ;
+		String ret = getConfigValue(key) ;
+		if (ret != null) {
+			retVal = Integer.parseInt(ret) ;
+		}
+		return retVal ;
+	}
+	
+	public static void main(String[] args) {
+
+		PasswordGenerator pg = new PasswordGenerator() ;
+		
+		if (args.length > 0) {
+			pg.setNoOfPasswordToGenerate(Integer.parseInt(args[0]));
+		}
+		
+		pg.printGeneratedPasswords() ;
+		
+	}
+	
+
 
 }
