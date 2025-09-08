0adc2fe4e860479b4c8da85927f37e5554bffeea
Selvamohan Neethiraj <selva@infotekies.com>
2025-09-08 11:35:56 -0400

    modified to serve new URL for password service

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,19 @@
-FROM infotekies/base-passwordgen
+FROM ubuntu:24.04
 ARG JAR_FILES=target/*.jar
 ARG PASSWORD_SERVICE=passwdservice
-ARG CONFIG_SRC_FILES=src/main/resources/*
+ARG CONFIG_SRC_FILES=./src/main/resources/*
 ARG INSTALL_ROOT=/home/passwordgen
 ARG LIB_FOLDER=${INSTALL_ROOT}/lib
 ARG CONF_FOLDER=${INSTALL_ROOT}/config
 ARG TOMCAT_LOGS_FOLDER=${INSTALL_ROOT}/tomcat/logs
+RUN apt-get -y update 
+RUN	apt-get -y install openjdk-21-jdk
 RUN mkdir -p ${LIB_FOLDER}
 RUN mkdir -p ${CONF_FOLDER}
 RUN mkdir -p ${TOMCAT_LOGS_FOLDER}
 COPY ${JAR_FILES} ${LIB_FOLDER}
 COPY ./docker/scripts/${PASSWORD_SERVICE} ${INSTALL_ROOT}/
-COPY ${CONFIG_SRC_FILES} ${CONF_FOLDER}
+COPY ${CONFIG_SRC_FILES} ${CONF_FOLDER}/
 EXPOSE 8080
 WORKDIR    ${INSTALL_ROOT}
 ENTRYPOINT [ "/home/passwordgen/passwdservice" ]
--- src/main/java/com/infotekies/passwdgen/PasswordController.java
+++ src/main/java/com/infotekies/passwdgen/PasswordController.java
@@ -12,6 +12,8 @@ import java.util.Set;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,17 +21,42 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PasswordController {
 
+  private static final Logger LOG = LoggerFactory.getLogger(PasswordController.class);
+
   @GetMapping(value = "/v1/{profileName}/passwords")
   public Set<String> getPasswords(@PathVariable String profileName) {
-    PasswordGenerator pg = new PasswordGenerator(profileName);
-    pg.init();
-    return pg.generatePassword();
+    Set<String> ret = null;
+    LOG.info("getPasswords called with profileName : [" + profileName + "]");
+    try {
+      PasswordGenerator pg = (profileName == null ? new PasswordGenerator() : new PasswordGenerator(profileName));
+      pg.init();
+      ret = pg.generatePassword();
+    } catch (Throwable t) {
+      LOG.error("Unable to generate passwords due to exception: ", t);
+    } finally {
+      int len = (ret == null ? 0 : ret.size());
+      LOG.info("getPasswords called with profileName : [" + profileName + "] returned [" + len + "] passwords.");
+    }
+    return ret;
+  }
+
+  @GetMapping(value = { "/", "/passwords" })
+  public Set<String> getPasswordsWithDefaultProfile() {
+    return getPasswords(null);
   }
 
   @GetMapping("/error")
   public String handleError(HttpServletRequest request) {
-    Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-    return (status != null ? ("error-" + status) : "error-unknown-status");
+    String ret = null;
+    try {
+      Object status = null;
+      if (request != null) {
+        status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+      }
+      ret = (status != null ? ("error-" + status) : "error-unknown-status");
+    } finally {
+      LOG.error("handleError : " + ret);
+    }
+    return ret;
   }
-
 }
